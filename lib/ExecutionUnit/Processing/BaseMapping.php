e9f81cc415d9bb02708a0309c2e66d3a66b5f550
Konstantin Ryapolov <kryapolov@gollard.ru>
2015-05-14 14:55:08 +0300

    add get and set helper for annotation helper

--- lib/ExecutionUnit/Processing/BaseMapping.php
+++ lib/ExecutionUnit/Processing/BaseMapping.php
@@ -39,8 +39,37 @@ class BaseMapping {
      *
      * @return array
      */
-    public function getState()
+    public function ExportState()
     {
         return get_object_vars($this);
+    }
+
+    /**
+     * #see http://php.net/manual/en/language.oop5.overloading.php
+     * @param $methodName
+     * @param $args
+     *
+     * @return mixed
+     * @throws MemberAccessException
+     */
+    public function __call($methodName, $args)
+    {
+        if (preg_match('~^(set|get)([A-Z])(.*)$~', $methodName, $matches)) {
+            $property = strtolower($matches[2]) . $matches[3];
+            if (!property_exists($this, $property)) {
+                throw new MemberAccessException('Property ' . $property . ' not exists');
+            }
+            switch ($matches[1]) {
+                case 'set':
+                    $this->$property = $args[0];
+
+                    return $this;
+                case 'get':
+                    return $this->$property;
+
+                case 'default':
+                    throw new MemberAccessException('Method ' . $methodName . ' not exists');
+            }
+        }
     }
 }
